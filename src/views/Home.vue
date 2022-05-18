70d069ba9be7ced0f7a36a4f87758fc3e9cd89d4
lixiaofeng <1130034920@qq.com>
2022-05-18 14:44:44 +0800

    add merge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <div>this is add</div>
+    <div>this is merge</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
